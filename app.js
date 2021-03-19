acf9503eac1ae13f43c31ab7731c2988f4b3e601
Ak705945 <72501596+Ak705945@users.noreply.github.com>
2021-03-19 19:04:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname+'/public'));
+
+app.get("/",function(req,res){
+  res.render("home")
+});
+
+app.get("/projects",function(req,res){
+  res.render("projects")
+})
+
+app.get("/certification",function(req,res){
+  res.render("certification")
+});
+
+app.get("/skills",function(req,res){
+  res.render("skills")
+})
+let port=process.env.PORT;
+if(port==null || port ==""){
+  port=3000;
+}
+app.listen(port , function() {
+  console.log("Server started");
+});
